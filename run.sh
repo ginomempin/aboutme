7b6eed53b22f0c2c6dff112c327eaceca2424ed4
gino <gino.mempin@gmail.com>
2022-02-06 12:09:19 +0900

    style: Decrease font size to 1.0em

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -e "static/css/home.css" ]
+then
+    python -mscss < assets/scss/home.scss > static/css/home.css
+fi
+
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 flask run
